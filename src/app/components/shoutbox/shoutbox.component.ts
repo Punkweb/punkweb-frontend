42d1cf1c14b2612612d93227f2f7b3c6ed66612d
Jordan Price <jprice@izeni.com>
2018-11-10 00:45:05 -0700

    Work on shoutbox

--- src/app/components/shoutbox/shoutbox.component.ts
+++ src/app/components/shoutbox/shoutbox.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
-import { ApiService, SanitizeService } from '../../services';
+import { ApiService, AuthService, SanitizeService } from '../../services';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-shoutbox',
@@ -9,24 +10,73 @@ import { ApiService, SanitizeService } from '../../services';
 })
 export class ShoutboxComponent implements OnInit, OnDestroy {
 
+  public moment = moment;
   public shouts = [];
-  private shoutsSub: Subscription = null;
+  public user = null;
+  public authenticated = false;
+  public shoutInput = '';
+
+  private reloadInterval = 30 * 1000;
+  private authSub: Subscription = null;
 
   constructor(
     private api: ApiService,
+    private auth: AuthService,
     public sanitize: SanitizeService,
   ) { }
 
   public ngOnInit() {
-    this.shoutsSub = this.api.Shout.paged().subscribe((shouts) => {
+    this.authSub = this.auth.user$.subscribe((user) => {
+      this.user = user;
+      this.authenticated = !!user.id;
+    });
+    this.getShouts().then((shouts: any) => {
       this.shouts = shouts;
+      setInterval(() => {
+        this.getShouts().then((shouts: any) => {
+          this.shouts = shouts;
+        });
+      });
     });
   }
 
   public ngOnDestroy() {
-    if (this.shoutsSub) {
-      this.shoutsSub.unsubscribe();
+    if (this.authSub) {
+      this.authSub.unsubscribe();
     }
   }
 
+  public getShouts() {
+    let promise = new Promise((resolve, reject) => {
+      let shoutsSub = this.api.Shout.paged().subscribe(
+        (shouts) => {
+          resolve(shouts);
+        },
+        (err) => {
+          reject(err);
+        },
+        () => {
+          shoutsSub.unsubscribe();
+        }
+      );
+    });
+    return promise;
+  }
+
+  public clickShout() {
+    let createSub = this.api.Shout.create({
+      content: this.shoutInput
+    }).subscribe(
+      (shout) => {
+        this.shoutInput = '';
+        this.getShouts().then((shouts: any) => {
+          this.shouts = shouts;
+        });
+      },
+      (err) => {},
+      () => {
+        createSub.unsubscribe();
+      }
+    );
+  }
 }
